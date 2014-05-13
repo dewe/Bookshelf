78687b5b93ee7bf35b80142caed02492f962d4e5
Johan Dewe <johan@dewe.net>
2014-05-13 22:47:57 +0200

    Add variations to sample data

--- API/App_Start/SampleData.cs
+++ API/App_Start/SampleData.cs
@@ -14,28 +14,32 @@ namespace API
                     Isbn = "0321125215",
                     Title = "Domain-Driven Design: Tackling Complexity in the Heart of Software",
                     Author = "Eric Evans",
-                    Loaned = null
+                    Loaned = null,
+                    Version = 21
                 },
                 new Book
                 {
                     Isbn = "0596805829",
                     Title = "REST in Practice, Hypermedia and Systems Architecture",
                     Author = "Jim Webber, Savas Parastatidis, Ian Robinson",
-                    Loaned = null
+                    Loaned = "Stefan",
+                    Version = 4
                 },
                 new Book
                 {
                     Isbn = "0670921602",
                     Title = "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
                     Author = "Eric Ries",
-                    Loaned = null
+                    Loaned = null,
+                    Version = 2
                 },
                 new Book
                 {
                     Isbn = "0321601912",
                     Title = "Continuous Delivery: Reliable Software Releases through Build, Test, and Deployment Automation",
                     Author = "Jez Humble, David Farley",
-                    Loaned = null
+                    Loaned = "Johan",
+                    Version = 42
                 }
             };
         }
