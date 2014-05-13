d03e6fbafe324095ed14084b802cc5a46613dad3
Johan Dewe <johan@dewe.net>
2014-05-13 22:19:12 +0200

    Add books_url to Root endpoint.

--- API/Controllers/RootController.cs
+++ API/Controllers/RootController.cs
@@ -10,11 +10,12 @@ namespace API.Controllers
             return new
             {
                 api_root_url = "http://bshelf.apphb.com",
+                bookshelf_url = "http://bshelf.apphb.com/books",
                 books_url = "http://bshelf.apphb.com/books/{isbn}",
                 loan_url = "http://bshelf.apphb.com/books/{isbn}/loan",
                 documentation_url = "http://docs.bshelf.apiary.io",
-                source_code_url = "https://github.com/dewe/Bookshelf",
-                design_comments_url = "https://github.com/dewe/Bookshelf/blob/master/Comments.md"
+                source_code_url = "https://github.com/dewe/bookshelf",
+                design_comments_url = "https://github.com/dewe/bookshelf/blob/master/Comments.md"
             };
         }
     }
